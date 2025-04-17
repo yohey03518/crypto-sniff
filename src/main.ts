6ec244cc8c6f7438160ce3e428fd844663172981
erwin <yohey03518@gmail.com>
2025-04-18 00:54:33 +0800

    init project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// Entry point for the crypto-sniff application
+
+async function main() {
+    console.log('Crypto-sniff application starting...');
+    // TODO: Add application logic here
+}
+
+// Execute the main function
+main().catch(error => {
+    console.error('Fatal error:', error);
+    process.exit(1);
+}); 
